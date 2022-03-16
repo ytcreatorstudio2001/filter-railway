76fccc1d6e00e6295c46980c7f3ad30c6f1bdca9
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-03-16 10:41:29 +0530

    Update abt_main_cb.py

--- bot/plugins/Callbacks/About Btn/abt_main_cb.py
+++ bot/plugins/Callbacks/About Btn/abt_main_cb.py
@@ -32,8 +32,8 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2">Source</a>"""
+    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/moviehub_plus">@mobiehub_plus</a>\n"""
+    text+="""\n<b><i>Source Code:</i></b> <a href="https://t.me/moviehub_plus">Source</a>"""
 
     buttons = [
         [
